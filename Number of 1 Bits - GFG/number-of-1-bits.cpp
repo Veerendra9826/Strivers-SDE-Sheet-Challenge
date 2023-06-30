1edbc869a11cdfb027910ca62da700bf2ac3e2de
Veerendra Singh Rajpoot <114106846+Veerendra9826@users.noreply.github.com>
2023-07-01 00:56:46 +0530

    Added solution - LeetHub

---
+++ Number of 1 Bits - GFG/number-of-1-bits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int temp = N;
+        int cnt = 0;
+        while(temp){
+            int last = temp%2;
+            if(last == 1){
+                cnt++;
+            }
+            temp = temp/2;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
