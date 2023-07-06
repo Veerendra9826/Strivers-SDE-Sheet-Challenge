702aa2b1f2dacbdf02e4694a63e262fd7f3f5855
Veerendra Singh Rajpoot <114106846+Veerendra9826@users.noreply.github.com>
2023-07-06 23:42:16 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+       //base case
+    if(low >= high) 
+        return ;
+
+    //partitioon karenfe
+    int p = partition(arr, low, high);
+
+    //left part sort karo
+    quickSort(arr, low, p-1);
+
+    //right wala part sort karo
+    quickSort(arr, p+1, high);
+
+           
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       int pivot  = arr[low];
+       int cnt = 0;
+       for(int i = low+1;i<=high;i++){
+           if(arr[i] <= pivot){
+               cnt++;
+           }
+       }
+      int pivotindex = low + cnt;
+      swap(arr[pivotindex],arr[low]);
+      
+      int i = low,j = high;
+      
+      while(i<pivotindex && j>pivotindex){
+          while(arr[i] <= pivot){
+              i++;
+          }
+          while(arr[j] > pivot){
+              j--;
+          }
+          
+          if(i<pivotindex && j>pivotindex){
+              swap(arr[i++],arr[j--]);
+          }
+      }
+      
+     return pivotindex;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
