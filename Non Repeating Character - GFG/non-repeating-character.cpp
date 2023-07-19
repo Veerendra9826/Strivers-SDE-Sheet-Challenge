fd5a8b2fe2605d63549e77817d1bb0248d924c27
Veerendra Singh Rajpoot <114106846+Veerendra9826@users.noreply.github.com>
2023-07-20 01:00:57 +0530

    Added solution - LeetHub

--- Non Repeating Character - GFG/non-repeating-character.cpp
+++ Non Repeating Character - GFG/non-repeating-character.cpp
@@ -11,16 +11,15 @@ class Solution
     //Function to find the first non-repeating character in a string.
     char nonrepeatingCharacter(string S)
     {
-        unordered_map<char,int>mp;
+        vector<int>v(26,0);
        for(int i = 0;i<S.size();i++){
-           mp[S[i]]++;
+           v[S[i] - 'a']++;
        }
        for(int i = 0;i<S.size();i++){
-           if(mp[S[i]] == 1){
+           if(v[S[i] - 'a'] == 1){
                return S[i];
            }
        }
-       
        return '$';
     }
 
